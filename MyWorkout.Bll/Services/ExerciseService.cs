e3bb21a26a1d1511475b86107bc411e60d9ba446
daniboy01 <pappdaniel012@gmail.com>
2021-05-07 16:50:05 +0200

    add images and ExerciseService

---
+++ MyWorkout.Bll/Services/ExerciseService.cs
@@ -0,0 +1,23 @@
+﻿using MyWorkout.Bll.Dto;
+using MyWorkout.Dal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWorkout.Bll.Services
+{
+    public class ExerciseService
+    {
+        public MyWorkoutDbContext DbContext { get; }
+        public ExerciseService( MyWorkoutDbContext dbContext )
+        {
+            DbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<ExerciseDto>> GetAllExerciseAsync()
+        {
+            var exercise = DbContext.Exercises.
+        }
+    }
+}

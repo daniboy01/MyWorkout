460be5ecf774371b8870b44bd283d3785c0ea02f
daniboy01 <pappdaniel012@gmail.com>
2021-05-09 18:47:49 +0200

    add user profile picture

--- MyWorkout.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ MyWorkout.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using MyWorkout.Dal.Entities;
 
 namespace MyWorkout.Web.Areas.Identity.Pages.Account.Manage
@@ -14,13 +18,24 @@ namespace MyWorkout.Web.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration config;
+        private readonly IWebHostEnvironment env;
+        private readonly long fileSizeLimit;
+        private List<string> permittedExtensions;
 
         public IndexModel(
             UserManager<User> userManager,
-            SignInManager<User> signInManager)
+            SignInManager<User> signInManager,
+            IConfiguration config,
+            IWebHostEnvironment env
+            )
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            this.config = config;
+            this.env = env;
+            this.fileSizeLimit = config.GetValue<long>("FileSizeLimit");
+            this.permittedExtensions = config.GetSection("PemittedExtensions").Get<List<string>>();
         }
 
         public string Username { get; set; }
@@ -30,6 +45,8 @@ namespace MyWorkout.Web.Areas.Identity.Pages.Account.Manage
 
         [BindProperty]
         public InputModel Input { get; set; }
+        [BindProperty]
+        public IFormFile CoverImage { get; set; }
 
         public class InputModel
         {
@@ -90,6 +107,27 @@ namespace MyWorkout.Web.Areas.Identity.Pages.Account.Manage
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
+
+            var fileName = CoverImage.FileName;
+            var ext = Path.GetExtension(fileName).ToLowerInvariant();
+
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+            {
+
+                ModelState.AddModelError("CoverImage", "Extension not permitted!");
+
+                return Page();
+            }
+
+            if (CoverImage != null && CoverImage.Length > 0)
+            {
+                var filePath = Path.Combine(this.env.WebRootPath, $"images/workoutPlan_covers/{user.Id}{ext}");
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await CoverImage.CopyToAsync(stream);
+                }
+            }
+
             return RedirectToPage();
         }
     }

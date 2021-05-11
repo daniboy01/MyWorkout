9c5798c3c0754590b263abdb9f8d002635ad4680
daniboy01 <pappdaniel012@gmail.com>
2021-05-11 17:13:12 +0200

    finish subscribe

--- MyWorkout.Web/Pages/WorkoutPlans/AddWorkout.cshtml.cs
+++ MyWorkout.Web/Pages/WorkoutPlans/AddWorkout.cshtml.cs
@@ -91,7 +91,7 @@ namespace MyWorkout.Web.Pages.WorkoutPlans
             await subsrciptionService.NotifySubscriptedUsers(
                     userId,
                     $"New workout has been uploaded by {User.FindFirstValue(ClaimTypes.Name)}",
-                    $"Checkout <a href='/WorkoutPlans/Details?Id={workout.Id}'></a>"
+                    $"Checkout <a href='localhost:44361/WorkoutPlans/Details?Id={workout.Id}'>clicking here</a>."
                 );
 
             return new RedirectToPageResult("/WorkoutPlans/Index");

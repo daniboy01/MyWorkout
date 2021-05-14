b018589623eb015424f2121f4d82f2bb6115e3eb
daniboy01 <pappdaniel012@gmail.com>
2021-05-14 10:41:52 +0200

    add blank image to workoutplans

--- MyWorkout.Web/Pages/WorkoutPlans/Index.cshtml.cs
+++ MyWorkout.Web/Pages/WorkoutPlans/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MyWorkout.Bll.Dto;
@@ -14,11 +15,14 @@ namespace MyWorkout.Web.Pages.WorkoutPlans
 {
     public class IndexModel : PageModel
     {
+        private readonly IWebHostEnvironment env;
+
         public WorkoutPlanService WorkoutPlanService { get; }
 
-        public IndexModel( WorkoutPlanService workoutPlanService )
+        public IndexModel( WorkoutPlanService workoutPlanService, IWebHostEnvironment env )
         {
             WorkoutPlanService = workoutPlanService;
+            this.env = env;
         }
 
         [BindProperty(SupportsGet = true)]
@@ -28,6 +32,15 @@ namespace MyWorkout.Web.Pages.WorkoutPlans
         public void OnGet()
         {
             WorkoutPlans = WorkoutPlanService.GetWorkouts(Specification);
+        }
+
+        public string LoadImage(int id)
+        {
+            if (System.IO.File.Exists($"{this.env.WebRootPath}/images/workoutPlan_covers/{id}.jpg"))
+            {
+                return $"{id}.jpg";
+            }
+            return "blank.png";
         }
     }
 }
